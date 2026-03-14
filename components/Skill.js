76e412ca00b0755c1cc05d8af87216b06afc8d45
Mo7amed-ragab <mhmdrjb455@gmail.com>
2026-03-14 04:05:36 +0200

    Update Tailwind CSS icon URL in Skill component to use the official CDN link.

--- components/Skill.js
+++ components/Skill.js
@@ -39,7 +39,7 @@ const skills = [
     id: 7,
     name: "Tailwind CSS",
     iconSvg:
-      "https://camo.githubusercontent.com/d0f8d43c038c7a1b9b70bfa4f41f20707ae83817ddc0245d0de9889fb5179f91/68747470733a2f2f63646e2e73696d706c6569636f6e732e6f72672f7461696c77696e646373732f303642364434",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
   },
   {
     id: 8,
