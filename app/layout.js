bdf57660f14d541a03aa4ece9951bb82643405b7
Mohamed Ibrahim <mhmdrjb455@gmail.com>
2026-05-24 18:21:37 +0300

    refactor: remove unused flaticon, slick, and nice-select assets and components

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,7 @@ import Preloader from "@/layout/Preloader";
 import WhatsappFloat from "@/components/WhatsappFloat";
 import "@css/animate.min.css";
 import "@css/bootstrap.min.css";
-import "@css/flaticon.min.css";
 import "@css/fontawesome-5.14.0.min.css";
-import "@css/nice-select.min.css";
-import "@css/slick.min.css";
 import "@css/style.css";
 import "./globals.css";
 
